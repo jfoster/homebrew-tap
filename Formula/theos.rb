dbfa37e3f25f717a96576d4f773497e789573f3f
jfoster <jacobfoster@pm.me>
2020-12-31 06:58:03 +0000

    theos: fix "Non-libraries were installed to..."

--- Formula/theos.rb
+++ Formula/theos.rb
@@ -1,7 +1,7 @@
 class Theos < Formula
   desc "Cross-platform suite of tools for building and deploying software for iOS"
   homepage "https://theos.dev"
-  url "https://github.com/theos/theos/archive/2.5.tar.gz"
+  url "https://github.com/theos/theos.git", tag: "2.5"
   sha256 "20539477e6d517917c93914d1c6f2920c172fb006139863675b8ac23505bede5"
   license "GPL-3.0-only"
   head "https://github.com/theos/theos.git"
@@ -14,6 +14,7 @@ class Theos < Formula
 
   def install
     prefix.install Dir["*"]
+    rm Dir["#{prefix}/*/.keep"]
   end
 
   test do
