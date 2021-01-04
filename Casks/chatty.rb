e1a29b5bed40eace47c6fad08675121e4a76b282
Jacob Foster <jacobfoster@pm.me>
2021-01-04 20:41:40 +0000

    fix problematic version numbers (#16)
    * fix problematic version numbers
    
    * fix style

--- Casks/chatty.rb
+++ Casks/chatty.rb
@@ -1,8 +1,8 @@
 cask "chatty" do
-  version "0.13.1,43"
+  version "0.13.1_mac43"
   sha256 "dec2fd80010a48f9b25814547fbccf29f969f3a287f71b6228a0685a776e37e9"
 
-  url "https://github.com/dehesselle/chatty/releases/download/v#{version.before_comma}_mac#{version.after_comma}/Chatty_v#{version.before_comma}_b#{version.after_comma}.dmg"
+  url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version.sub(/mac/, "b")}.dmg"
   appcast "https://github.com/dehesselle/chatty/releases.atom"
   name "Chatty"
   desc "Twitch Chat Client for Desktop"
