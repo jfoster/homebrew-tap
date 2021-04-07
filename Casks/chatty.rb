612737536f701c236e51ec4fce9a9a30958b0f08
Jacob Foster <jacobfoster@pm.me>
2021-04-07 12:22:08 +0100

    chatty 0.13.1_mac44 (#34)

--- Casks/chatty.rb
+++ Casks/chatty.rb
@@ -1,8 +1,8 @@
 cask "chatty" do
-  version "0.13.1_mac43"
+  version "0.13.1_mac44"
   sha256 "dec2fd80010a48f9b25814547fbccf29f969f3a287f71b6228a0685a776e37e9"
 
-  url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version.sub(/mac/, "b")}.dmg"
+  url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}.dmg"
   appcast "https://github.com/dehesselle/chatty/releases.atom"
   name "Chatty"
   desc "Twitch Chat Client for Desktop"
