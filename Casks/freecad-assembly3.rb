968bb0d71ea810e7e567adfd3fbec9d7d289a8ec
jfoster <jacobfoster@pm.me>
2021-04-23 11:56:28 +0100

    freecad-assembly3 0.11,20210412 (new cask)

---
+++ Casks/freecad-assembly3.rb
@@ -0,0 +1,23 @@
+cask "freecad-assembly3" do
+  version "0.11,20210412"
+  sha256 :no_check # required as upstream package is updated in-place
+
+  url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/#{version.before_comma}/FreeCAD-asm3-Stable-OSX-Conda-Py3-Qt5-#{version.after_comma}-x86_64.dmg"
+  name "FreeCAD Assembly3"
+  desc "3D parametric modeler"
+  homepage "https://github.com/realthunder/FreeCAD_assembly3"
+
+  livecheck do
+    url "https://github.com/realthunder/FreeCAD_assembly3/releases"
+    strategy :page_match do |page|
+      match = page.match(
+        %r{href=.*?/(\d+(?:\.\d+)*)/FreeCAD-asm3-Stable-.*?-(\d+?)-x86_64\.dmg}i,
+      )
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  depends_on macos: ">= :sierra"
+
+  app "FreeCAD.app", target: "FreeCADLink.app"
+end
