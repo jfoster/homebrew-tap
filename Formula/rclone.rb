2ddbc978a013da208c7ec8ee6ae9daf5fd4f3ee5
jfoster <jacobfoster@pm.me>
2021-04-27 13:13:56 +0100

    Don't do special steps to bottle on mac

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -32,26 +32,20 @@ class FuseRequirement < Requirement
   end
 end
 
+  
 class Rclone < Formula
   desc "Rsync for cloud storage"
   homepage "https://rclone.org/"
   url "https://github.com/rclone/rclone/archive/v1.55.1.tar.gz"
   sha256 "b8cbf769c8ed41c6e1dd74de78bf14ee7935ee436ee5ba018f742a48ee326f62"
   license "MIT"
+  revision 1
   head "https://github.com/rclone/rclone.git"
 
-  bottle do
-    root_url "https://github.com/jfoster/homebrew-tap/releases/download/rclone-1.55.1"
-    sha256 cellar: :any_skip_relocation, big_sur:      "a1f962f9f642b719580f09e8cdf7e29388ff7279a1ce394bb0ad969b857cccd6"
-    sha256 cellar: :any_skip_relocation, catalina:     "ebce286d24f193a11669d58cee52a9bab448cd91b7f957ffa07b9f9d52b3f910"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "af4721178385164d23e5a3df593bc8fe9706bd0766bb50f88017f3d68679f54b"
-  end
-
-  depends_on "go" => :build
   depends_on FuseRequirement unless OS.linux?
+  depends_on "go" => :build
 
   def install
-    # ENV.deparallelize
     args = *std_go_args
     on_macos do
       args += ["-tags", "cmount"]
@@ -66,14 +60,17 @@ class Rclone < Formula
     zsh_completion.install "_rclone"
   end
 
-  # def caveats
-  #   <<~EOS
-  #   EOS
-  # end
+  def caveats
+    <<~EOS
+      Homebrew's installation does not include the `mount` subcommand on MacOS.
+    EOS
+  end
 
   test do
     (testpath/"file1.txt").write "Test!"
     system "#{bin}/rclone", "copy", testpath/"file1.txt", testpath/"dist"
     assert_match File.read(testpath/"file1.txt"), File.read(testpath/"dist/file1.txt")
   end
+end
+
 end
