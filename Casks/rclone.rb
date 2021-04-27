443de08f3973de229d9d9d272e700352c8370464
jfoster <jacobfoster@pm.me>
2021-04-27 10:57:23 +0100

    rclone 1.55.1 (new cask)

---
+++ Casks/rclone.rb
@@ -0,0 +1,13 @@
+cask "rclone" do
+  version "1.55.1"
+  sha256 "a226d27b749d8376ceb696401bd3186e9942d5ed055aba2a37cff5d835aa510a"
+
+  url "https://downloads.rclone.org/v#{version}/rclone-v#{version}-osx-amd64.zip"
+  name "rclone"
+  desc "Command line program to manage files on cloud storage."
+  homepage "https://rclone.org"
+
+  conflicts_with formula: "rclone"
+
+  binary "rclone-v#{version}-osx-amd64/rclone"
+end
