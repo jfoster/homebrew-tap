17bf717563deadbf80f2fca345ed4efef12c926b
jfoster <jacobfoster@pm.me>
2021-04-27 11:06:41 +0100

    rclone: update and verify github url

--- Casks/rclone.rb
+++ Casks/rclone.rb
@@ -2,7 +2,8 @@ cask "rclone" do
   version "1.55.1"
   sha256 "a226d27b749d8376ceb696401bd3186e9942d5ed055aba2a37cff5d835aa510a"
 
-  url "https://downloads.rclone.org/v#{version}/rclone-v#{version}-osx-amd64.zip"
+  url "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-osx-amd64.zip",
+      verified: "github.com/rclone/rclone"
   name "rclone"
   desc "Command line program to manage files on cloud storage."
   homepage "https://rclone.org"
