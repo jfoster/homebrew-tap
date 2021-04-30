c7928e6f47cfbb2b8229d99c1401529c7be90510
jfoster <jacobfoster@pm.me>
2021-04-30 11:03:19 +0100

    discord-slowmode-bot: add test

--- Formula/discord-slowmode-bot.rb
+++ Formula/discord-slowmode-bot.rb
@@ -4,7 +4,7 @@ class DiscordSlowmodeBot < Formula
   url "https://github.com/jfoster/discord-slowmode-bot/archive/v0.2.0.tar.gz"
   sha256 "3f940e859df9c4d049ab0797d0486abfcad4739c0419e7c71c915f34a9b2fa93"
   license "MIT"
-  revision 2
+  revision 3
   head "https://github.com/jfoster/discord-slowmode-bot.git"
 
   bottle do
@@ -17,19 +17,12 @@ class DiscordSlowmodeBot < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./cmd/discord-slowmode-bot"
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/discord-slowmode-bot"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test discord-slowmode-bot`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
+    version_output = shell_output("#{bin}/discord-slowmode-bot --version")
+    assert_match "discord-slowmode-bot version #{version}", version_output
   end
 end
