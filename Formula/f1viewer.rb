b084dc482f13381edd84a468bfebd666241e12f0
Jacob Foster <jacobfoster@pm.me>
2021-05-03 02:06:15 +0000

    f1viewer 2.0.4
    Closes #57.
    
    Signed-off-by: Jacob Foster <jacobfoster@pm.me>

--- Formula/f1viewer.rb
+++ Formula/f1viewer.rb
@@ -1,8 +1,8 @@
 class F1viewer < Formula
   desc "🏎️ TUI for F1TV"
   homepage "https://github.com/SoMuchForSubtlety/f1viewer"
-  url "https://github.com/SoMuchForSubtlety/f1viewer/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "0df83a5f657107559586217ea71c460c97dc730785cc14fd8ebf0e75cd1914a9"
+  url "https://github.com/SoMuchForSubtlety/f1viewer/archive/refs/tags/v2.0.4.tar.gz"
+  sha256 "1ca21302bac36467d9da175495e85fffa33c0b4b842dfe95d597ae6d01f6403a"
   license "GPL-3.0-only"
 
   bottle do
