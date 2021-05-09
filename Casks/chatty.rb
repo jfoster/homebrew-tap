45620d21f1341d0c6ed8743d3513b86620367746
jfoster <jacobfoster@pm.me>
2021-05-10 00:31:11 +0100

    chatty: remove appcast

--- Casks/chatty.rb
+++ Casks/chatty.rb
@@ -3,7 +3,6 @@ cask "chatty" do
   sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
 
   url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}.dmg"
-  appcast "https://github.com/dehesselle/chatty/releases.atom"
   name "Chatty"
   desc "Twitch Chat Client for Desktop"
   homepage "https://github.com/dehesselle/chatty"
