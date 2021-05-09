be227fe662ab716d827a57534f1fb191e90d8712
jfoster <jacobfoster@pm.me>
2021-05-10 00:33:21 +0100

    spek: remove appcast

--- Casks/spek.rb
+++ Casks/spek.rb
@@ -3,7 +3,6 @@ cask "spek" do
   sha256 "6426c4c34ac8bcb2fa1020cb295e3c105ca912c853cbbe84d1d196a32bba361b"
 
   url "https://github.com/withmorten/spek-alternative/releases/download/#{version}/spek-alternative-#{version}.dmg"
-  appcast "https://github.com/withmorten/spek-alternative/releases.atom"
   name "Spek"
   desc "Acoustic spectrum analyser"
   homepage "https://github.com/withmorten/spek-alternative"
