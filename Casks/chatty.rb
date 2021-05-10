60e61f789ddbc3a6e9fdf50ee1224b531e7565fb
Jacob Foster <jacobfoster@pm.me>
2021-05-10 01:40:21 +0100

    chatty: bigsur only version (#61)
    * chatty: bigsur only version
    
    * chatty: brew style --fix

--- Casks/chatty.rb
+++ Casks/chatty.rb
@@ -1,8 +1,14 @@
 cask "chatty" do
-  version "0.14_mac45"
-  sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
+  if Hardware::CPU.intel?
+    version "0.14_mac45"
+    sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
+    url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}.dmg"
+  else
+    version "0.14_mac46"
+    sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
+    url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}_arm64.dmg"
+  end
 
-  url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}.dmg"
   name "Chatty"
   desc "Twitch Chat Client for Desktop"
   homepage "https://github.com/dehesselle/chatty"
