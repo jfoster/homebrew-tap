4ab565b268533f4e2be432e353bb694ad07a37e1
Jacob Foster <jacobfoster@pm.me>
2021-06-25 01:57:05 +0000

    f1viewer 2.1.0

--- Formula/f1viewer.rb
+++ Formula/f1viewer.rb
@@ -1,10 +1,9 @@
 class F1viewer < Formula
   desc "🏎️ TUI for F1TV"
   homepage "https://github.com/SoMuchForSubtlety/f1viewer"
-  url "https://github.com/SoMuchForSubtlety/f1viewer/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "0df83a5f657107559586217ea71c460c97dc730785cc14fd8ebf0e75cd1914a9"
+  url "https://github.com/SoMuchForSubtlety/f1viewer/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "59354ef3b1ec29e28b845709558270b745ef473a086e550f09598572fb2efa62"
   license "GPL-3.0-only"
-  revision 2
   head "https://github.com/SoMuchForSubtlety/f1viewer.git"
 
   livecheck do
