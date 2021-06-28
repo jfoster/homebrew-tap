e19953612497a15adca3bde21de5cf9968e24c75
Jacob Foster <jacobfoster@pm.me>
2021-06-28 01:59:28 +0000

    deemix 3.0.0
    Closes #77.
    
    Signed-off-by: Jacob Foster <jacobfoster@pm.me>

--- Formula/deemix.rb
+++ Formula/deemix.rb
@@ -3,10 +3,9 @@ class Deemix < Formula
 
   desc "Deezer downloader"
   homepage "https://www.reddit.com/r/deemix"
-  url "https://files.pythonhosted.org/packages/18/86/98c1deb990954a40e802d6fd1c4e03909ee82b9be23885b7977d2b4a3936/deemix-2.0.16.tar.gz"
-  sha256 "09829c6e56fc9d14e999d3d4a8ec59c45f616596be08c19f985d28404affb660"
+  url "https://files.pythonhosted.org/packages/07/fe/8a0b6de8553ad615c2b9c268b0012ebbe7373b564103d30a76be60c4eaa2/deemix-3.0.0.tar.gz"
+  sha256 "efc708062719a3d4e024524697d86990fdb465e5d4098389a951d27633073c02"
   license "GPL-3.0-only"
-  revision 1
 
   bottle do
     root_url "https://github.com/jfoster/homebrew-tap/releases/download/deemix-2.0.16_1"
@@ -18,8 +17,8 @@ class Deemix < Formula
   depends_on "python@3.8"
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/06/a9/cd1fd8ee13f73a4d4f491ee219deeeae20afefa914dfb4c130cfc9dc397a/certifi-2020.12.5.tar.gz"
-    sha256 "1a4995114262bffbc2413b159f2a1a480c969de6e6eb13ee966d470af86af59c"
+    url "https://files.pythonhosted.org/packages/6d/78/f8db8d57f520a54f0b8a438319c342c61c22759d8f9a1cd2e2180b5e5ea9/certifi-2021.5.30.tar.gz"
+    sha256 "2bbf76fd432960138b3ef6dda3dde0544f27cbf8546c458e60baf371917ba9ee"
   end
 
   resource "chardet" do
@@ -28,28 +27,13 @@ class Deemix < Formula
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz"
-    sha256 "d2b5255c7c6349bc1bd1e59e08cd12acbbd63ce649f2588755783aa94dfb6b1a"
+    url "https://files.pythonhosted.org/packages/21/83/308a74ca1104fe1e3197d31693a7a2db67c2d4e668f20f43a2fca491f9f7/click-8.0.1.tar.gz"
+    sha256 "8c04c11192119b1ef78ea049e0a6f0463e4c48ef00a30160c704337586f3ad7a"
   end
 
   resource "deezer-py" do
-    url "https://files.pythonhosted.org/packages/db/57/4ae4f62284a284795e79e46bb3a173bff58cfd8ad871f2da30321449bd65/deezer-py-0.0.15.tar.gz"
-    sha256 "da0fc66c9fd7333c93d920c66815e47f42d9ade6efa85cd6f321ac101ccae469"
-  end
-
-  resource "dnspython" do
-    url "https://files.pythonhosted.org/packages/ec/c5/14bcd63cb6d06092a004793399ec395405edf97c2301dfdc146dfbd5beed/dnspython-1.16.0.zip"
-    sha256 "36c5e8e38d4369a08b6780b7f27d790a292b2b08eea01607865bf0936c558e01"
-  end
-
-  resource "eventlet" do
-    url "https://files.pythonhosted.org/packages/40/9c/bd7bc0202a84012a4b6b653b54a389ef48bc7f13ce628865357ffdf37160/eventlet-0.30.1.tar.gz"
-    sha256 "d00649a7e17de0bcddff1a96311ed3baf1b295b3223d4b71aceafe7b45e6d6f8"
-  end
-
-  resource "greenlet" do
-    url "https://files.pythonhosted.org/packages/92/be/878cc5314fa5aadce33e68738c1a24debe317605196bdfc2049e66bc9c30/greenlet-1.0.0.tar.gz"
-    sha256 "719e169c79255816cdcf6dccd9ed2d089a72a9f6c42273aae12d55e8d35bdcf8"
+    url "https://files.pythonhosted.org/packages/f4/47/465be6d2ebe401b62dd4ffe4df9b10746bdc5fdb0f95c12b42b8d2c15d40/deezer-py-1.0.0.tar.gz"
+    sha256 "b8f0a490613f7a1f3f9c53a59267e4faa0504f7baf5ed72d7a9625449f83751e"
   end
 
   resource "idna" do
@@ -73,18 +57,18 @@ class Deemix < Formula
   end
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
-    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
+    url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   end
 
   resource "spotipy" do
-    url "https://files.pythonhosted.org/packages/53/f0/d4605b887e01686d01dc1c1d25e8d13af8a12bf1dc1ed3c87e9bd909b49d/spotipy-2.16.1.tar.gz"
-    sha256 "4564a6b05959deb82acc96a3fe6883db1ad9f8c73b7ff3b9f1f44db43feba0b8"
+    url "https://files.pythonhosted.org/packages/50/0d/dcb5fd830c2a2689ea17a0ae98d29c6ac9be0937fca2779645531a66b796/spotipy-2.18.0.tar.gz"
+    sha256 "f7293b808696807e9acec6bdcff63f7dcc3cc1b148c0c4b4299ef43c966f7177"
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/d7/8d/7ee68c6b48e1ec8d41198f694ecdc15f7596356f2ff8e6b1420300cf5db3/urllib3-1.26.3.tar.gz"
-    sha256 "de3eedaad74a2683334e282005cd8d7f22f4d55fa690a2a1020a416cb0a47e73"
+    url "https://files.pythonhosted.org/packages/4f/5a/597ef5911cb8919efe4d86206aa8b2658616d676a7088f0825ca08bd7cb8/urllib3-1.26.6.tar.gz"
+    sha256 "f57b4c16c62fa2760b7e3d97c35b255512fb6b59a259730f36ba32ce9f8e342f"
   end
 
   def install
