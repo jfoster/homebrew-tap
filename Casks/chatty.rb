d59a13930c11e361e209301d61d7e3737f4f710e
jfoster <jacobfoster@pm.me>
2021-07-29 23:04:23 +0100

    chatty: fix livecheck

--- Casks/chatty.rb
+++ Casks/chatty.rb
@@ -1,29 +1,23 @@
 cask "chatty" do
+  version "0.14_mac45"
+
   if Hardware::CPU.intel?
-    version "0.14_mac45"
     sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
-    url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}.dmg"
-
-    livecheck do
-      url "https://github.com/dehesselle/chatty/releases"
-      strategy :page_match
-      regex(%r{href=.*?/Chatty_v(\d+(?:\.\d+)+_mac\d+)\.dmg}i)
-    end
+    url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}_x64.dmg"
   else
-    version "0.14_mac46"
-    sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
     url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}_arm64.dmg"
-
-    livecheck do
-      url "https://github.com/dehesselle/chatty/releases"
-      strategy :page_match
-      regex(%r{href=.*?/Chatty_v(\d+(?:\.\d+)+_mac\d+)_arm64\.dmg}i)
-    end
+    sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
   end
 
   name "Chatty"
   desc "Twitch Chat Client for Desktop"
   homepage "https://github.com/dehesselle/chatty"
+
+  livecheck do
+    url "https://github.com/dehesselle/chatty/releases"
+    strategy :page_match
+    regex(%r{href=.*?/Chatty_v(\d+(?:\.\d+)+_mac\d+)_arm64\.dmg}i)
+  end
 
   app "Chatty.app"
 end
