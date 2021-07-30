d847b774d10ba18b7fd492ed83e2ae512907a2ba
jfoster <jacobfoster@pm.me>
2021-07-30 02:33:44 +0100

    chatty: fix sha

--- Casks/chatty.rb
+++ Casks/chatty.rb
@@ -2,11 +2,11 @@ cask "chatty" do
   version "0.14_mac45"
 
   if Hardware::CPU.intel?
-    sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
+    sha256 "6132a96127eed0148ecfc314ba555eb54fef9057b7e780a641a9841170053435"
     url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}_x64.dmg"
   else
+    sha256 "56222ee217afc7e1f7e12fc6d20538dc276b5be7b5746be55b7c98377d0ffc28"
     url "https://github.com/dehesselle/chatty/releases/download/v#{version}/Chatty_v#{version}_arm64.dmg"
-    sha256 "7d021002ae5088ae660b11b9621687921bd7de93a955b71718f26c93924db7e4"
   end
 
   name "Chatty"
