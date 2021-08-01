7e2f3ffad0feb7c51b194595ffb288b938b98491
jfoster <jacobfoster@pm.me>
2021-08-02 00:12:34 +0100

    f1viewer: fix build

--- Formula/f1viewer.rb
+++ Formula/f1viewer.rb
@@ -21,7 +21,7 @@ class F1viewer < Formula
 
   def install
     ldflags = "-s -w -X main.version=#{version}"
-    system "go", "build", *std_go_args, "-ldflags", ldflags, "./cmd/main.go"
+    system "go", "build", *std_go_args, "-ldflags", ldflags
   end
 
   test do
