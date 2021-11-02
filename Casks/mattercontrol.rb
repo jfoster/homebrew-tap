ef9e9decd460fea97b446a694dd63c492d116fff
jfoster <jacobfoster@pm.me>
2021-11-02 23:08:42 +0000

    mattercontrol: fix description style offences

--- Casks/mattercontrol.rb
+++ Casks/mattercontrol.rb
@@ -5,7 +5,7 @@ cask "mattercontrol" do
   url "https://mattercontrol.appspot.com/downloads/mattercontrol-mac-os-x/release",
       verified: "mattercontrol.appspot.com"
   name "mattercontrol"
-  desc "A free, open-source, all-in-one software package that lets you design, slice, organize, and manage your 3D prints"
+  desc "All-in-one software package that lets you manage your 3D prints"
   homepage "https://www.matterhackers.com/store/l/mattercontrol/sk/MKZGTDW6"
 
   pkg "MatterControlSetup.pkg"
