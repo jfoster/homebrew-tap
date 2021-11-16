1cae3f6a863f0e700c28824ec4d21209988e85d4
jfoster <jacobfoster@pm.me>
2021-11-16 11:40:51 +0000

    steamguard-cli v0.4.2 (new formula)

---
+++ Formula/steamguard-cli.rb
@@ -0,0 +1,24 @@
+class SteamguardCli < Formula
+  desc "Utility for generating Steam 2FA codes and managing trade confirmations"
+  homepage "https://github.com/dyc3/steamguard-cli"
+  url "https://github.com/dyc3/steamguard-cli/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "8491cd80b6515c4907a67f8aa4984d01057239bb074554b12386dfac35a92014"
+  license "GPL-3.0-only"
+  head "https://github.com/dyc3/steamguard-cli.git", branch: "master"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    version_output = shell_output("#{bin}/steamguard-cli --version")
+    assert_match "steamguard-cli #{version}", version_output
+  end
+end
