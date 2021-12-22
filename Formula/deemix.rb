9f2686b594860e5b0683481fe45a03dc6843a608
Jacob Foster <jacobfoster@pm.me>
2021-12-22 02:01:52 +0000

    deemix 3.5.7

--- Formula/deemix.rb
+++ Formula/deemix.rb
@@ -3,8 +3,8 @@ class Deemix < Formula
 
   desc "Deezer downloader"
   homepage "https://www.reddit.com/r/deemix"
-  url "https://files.pythonhosted.org/packages/4a/57/0be002e70d5ae51d8e3aff603cfd9e78cc29d2b0e5c54ec509d6d4c7677d/deemix-3.5.5.tar.gz"
-  sha256 "a9ab6d50a746afd3f66a5e5c89b1b408f24d9950898e013e86e70eb65ee90211"
+  url "https://files.pythonhosted.org/packages/56/97/d04c6f7c3dfa6f072a656593a39b25ed03babd42096f05f3630dfbe0961d/deemix-3.5.7.tar.gz"
+  sha256 "3ce3a2c95e86eec57b8055a34b7c70b3e3a5e0b9fd41a2d83de12ae19ec6ea69"
   license "GPL-3.0-only"
 
   bottle do
@@ -16,28 +16,28 @@ class Deemix < Formula
   depends_on "python@3.8"
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/6d/78/f8db8d57f520a54f0b8a438319c342c61c22759d8f9a1cd2e2180b5e5ea9/certifi-2021.5.30.tar.gz"
-    sha256 "2bbf76fd432960138b3ef6dda3dde0544f27cbf8546c458e60baf371917ba9ee"
+    url "https://files.pythonhosted.org/packages/6c/ae/d26450834f0acc9e3d1f74508da6df1551ceab6c2ce0766a593362d6d57f/certifi-2021.10.8.tar.gz"
+    sha256 "78884e7c1d4b00ce3cea67b44566851c4343c120abd683433ce934a68ea58872"
   end
 
   resource "charset-normalizer" do
-    url "https://files.pythonhosted.org/packages/eb/7f/a6c278746ddbd7094b019b08d1b2187101b1f596f35f81dc27f57d8fcf7c/charset-normalizer-2.0.6.tar.gz"
-    sha256 "5ec46d183433dcbd0ab716f2d7f29d8dee50505b3fdb40c6b985c7c4f5a3591f"
+    url "https://files.pythonhosted.org/packages/68/e4/e014e7360fc6d1ccc507fe0b563b4646d00e0d4f9beec4975026dd15850b/charset-normalizer-2.0.9.tar.gz"
+    sha256 "b0b883e8e874edfdece9c28f314e3dd5badf067342e42fb162203335ae61aa2c"
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/21/83/308a74ca1104fe1e3197d31693a7a2db67c2d4e668f20f43a2fca491f9f7/click-8.0.1.tar.gz"
-    sha256 "8c04c11192119b1ef78ea049e0a6f0463e4c48ef00a30160c704337586f3ad7a"
+    url "https://files.pythonhosted.org/packages/f4/09/ad003f1e3428017d1c3da4ccc9547591703ffea548626f47ec74509c5824/click-8.0.3.tar.gz"
+    sha256 "410e932b050f5eed773c4cda94de75971c89cdb3155a72a0831139a79e5ecb5b"
   end
 
   resource "deezer-py" do
-    url "https://files.pythonhosted.org/packages/d0/6e/5db2c495e802d4a3641804c5194c2eb67d0787a5f22b3d535a97688cc9bf/deezer-py-1.2.5.tar.gz"
-    sha256 "25c7b23014052c3ddf44f6fd9c994639237b8800ae246f1d9a515f3a186c60a7"
+    url "https://files.pythonhosted.org/packages/f0/d0/e1a7ee400e4fd791fce9de3aa7a5e900962dd5daa860f086c5817610bc31/deezer-py-1.2.9.tar.gz"
+    sha256 "16eacdc9c53f55d2130891c6335e88046afa0601184fd5197fee35a09e99d9cf"
   end
 
   resource "idna" do
-    url "https://files.pythonhosted.org/packages/cb/38/4c4d00ddfa48abe616d7e572e02a04273603db446975ab46bbcd36552005/idna-3.2.tar.gz"
-    sha256 "467fbad99067910785144ce333826c71fb0e63a425657295239737f7ecd125f3"
+    url "https://files.pythonhosted.org/packages/62/08/e3fc7c8161090f742f504f40b1bccbfc544d4a4e09eb774bf40aafce5436/idna-3.3.tar.gz"
+    sha256 "9d643ff0a55b762d5cdb124b8eaa99c66322e2157b69160bc32796e824360e6d"
   end
 
   resource "mutagen" do
@@ -46,8 +46,8 @@ class Deemix < Formula
   end
 
   resource "pycryptodomex" do
-    url "https://files.pythonhosted.org/packages/a3/33/1e5040f7c82bce5db00a4101c159339263069717b1155b4b8fa710d19721/pycryptodomex-3.10.4.tar.gz"
-    sha256 "acd3498a8ccf8ad20ce7daa5f7f5e6521465eeceb026f28a6adb0228dd9fcc6e"
+    url "https://files.pythonhosted.org/packages/f6/06/e2ad9e93210790be86d36c6e2d5524ba54928c3ed27dd0be9b2ced7c57f1/pycryptodomex-3.12.0.zip"
+    sha256 "922e9dac0166e4617e5c7980d2cff6912a6eb5cb5c13e7ece222438650bd7f66"
   end
 
   resource "requests" do
