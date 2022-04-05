de3f54ba04eb2b27080b4b1723e8a87dd5e19e6a
Jacob Foster <jacobfoster@pm.me>
2022-04-05 22:26:52 +0100

    Update freecad-assembly3.rb

--- Casks/freecad-assembly3.rb
+++ Casks/freecad-assembly3.rb
@@ -3,10 +3,10 @@ cask "freecad-assembly3" do
 
   if MacOS.version <= :catalina
     sha256 "102a0039bbd25c8dcaa9cbc3f94712f8c0ab9d58e9ffd7da25ad233e7f2c03f3"
-    url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/#{version.before_comma}/FreeCAD-asm3-Stable-OSX-Conda-Py3-Qt5-#{version.after_comma}-x86_64.dmg"
+    url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/#{version.before_comma}/FreeCAD-asm3-Daily-OSX-Conda-Py3-Qt5-#{version.after_comma}-x86_64.dmg"
   else
     sha256 "a10c9a6fcc114a1e6ac8ab0d64624bc438555e1cf540fa14c831e454b2442b6d"
-    url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/#{version.before_comma}/FreeCAD-asm3-Stable-BigSur-OSX-Py3-Qt5-#{version.after_comma}-x86_64.dmg"
+    url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/#{version.before_comma}/FreeCAD-asm3-Daily-BigSur-OSX-Py3-Qt5-#{version.after_comma}-x86_64.dmg"
   end
 
   name "FreeCAD Assembly3"
@@ -17,7 +17,7 @@ cask "freecad-assembly3" do
     url "https://github.com/realthunder/FreeCAD_assembly3/releases/latest"
     strategy :page_match do |page|
       match = page.match(
-        %r{href=.*?/(\d+(?:\.\d+)*)/FreeCAD-asm3-Stable-.*?-(\d+?)-x86_64\.dmg}i,
+        %r{href=.*?/(\d+(?:\.\d+)*)/FreeCAD-asm3-.*?-(\d+?)-x86_64\.dmg}i,
       )
       "#{match[1]},#{match[2]}"
     end
