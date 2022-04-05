fd80a0305717d11e8016f91d56fb4335c70da457
Jacob Foster <jacobfoster@pm.me>
2022-04-05 21:32:55 +0000

    Update freecad-assembly3 from 0.11,20220111 to 0.11,20220405

--- Casks/freecad-assembly3.rb
+++ Casks/freecad-assembly3.rb
@@ -1,11 +1,11 @@
 cask "freecad-assembly3" do
-  version "0.11,20220111"
+  version "0.11,20220405"
 
   if MacOS.version <= :catalina
     sha256 "102a0039bbd25c8dcaa9cbc3f94712f8c0ab9d58e9ffd7da25ad233e7f2c03f3"
     url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/#{version.before_comma}/FreeCAD-asm3-Daily-OSX-Conda-Py3-Qt5-#{version.after_comma}-x86_64.dmg"
   else
-    sha256 "a10c9a6fcc114a1e6ac8ab0d64624bc438555e1cf540fa14c831e454b2442b6d"
+    sha256 "48e757dc5fa136eba5a5258635a9b942b660e7b48d08ebd79ddeb3ba10109a7d"
     url "https://github.com/realthunder/FreeCAD_assembly3/releases/download/#{version.before_comma}/FreeCAD-asm3-Daily-BigSur-OSX-Py3-Qt5-#{version.after_comma}-x86_64.dmg"
   end
 
