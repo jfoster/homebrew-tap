e5835faccb02098b637395b93efe6d27618affa2
jfoster <jacobfoster@pm.me>
2022-05-03 11:23:56 +0100

    fix style

--- Casks/freecad.rb
+++ Casks/freecad.rb
@@ -2,7 +2,7 @@ cask "freecad" do
   version "2022.04.30-edge"
   sha256 "ece919c8551e8624a7fbda92d940d636d2e3e69924baecf0fb9634ed9b4c3992"
 
-  url "https://github.com/realthunder/FreeCAD/releases/download/#{version}/FreeCAD-asm3-Stable-OSX-Conda-Py3-Qt5-#{version.delete('.').delete('-edge')}-x86_64.dmg"
+  url "https://github.com/realthunder/FreeCAD/releases/download/#{version}/FreeCAD-asm3-Stable-OSX-Conda-Py3-Qt5-#{version.delete(".").delete("-edge")}-x86_64.dmg"
   name "FreeCAD"
   desc "3D parametric modeler"
   homepage "https://github.com/realthunder/FreeCAD"
