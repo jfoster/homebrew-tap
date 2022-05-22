3c43daa9588841b98db35b44651cb406b6a9d3a8
jfoster <jacobfoster@pm.me>
2022-05-22 09:39:17 +0100

    r128gain v1.0.6 (new formula)

---
+++ Formula/r128gain.rb
@@ -0,0 +1,20 @@
+class R128gain < Formula
+  include Language::Python::Virtualenv
+
+  desc "Fast audio loudness scanner & tagger"
+  homepage "https://github.com/desbma/r128gain"
+  url "https://github.com/desbma/r128gain/archive/refs/tags/1.0.6.tar.gz"
+  sha256 "1aa20b360b845dec303964d3ba1ab2f5361ece465586ca74862ecee1c69223cc"
+  license "LGPL-2.1"
+
+  depends_on "python"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "true"
+  end
+end
