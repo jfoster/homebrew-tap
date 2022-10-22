0abb7e4f5eafc7d6eb5986b1c89f3b43faaa5a12
jfoster <jacobfoster@pm.me>
2022-10-22 06:42:15 +0100

    fix

--- Casks/launchpadder.rb
+++ Casks/launchpadder.rb
@@ -2,9 +2,10 @@ cask "launchpadder" do
   version "1.4"
   sha256 "e6f22aec28546b3ab418cb06c9a26c696126c4ec4daeeaebd1a427a8c8b86390"
 
-  url "https://github.com/Molay/LaunchPadder/releases/download/v#{version}/LaunchPadder-v#{version}.zip"
+  url "https://github.com/Molay/LaunchPadder/releases/download/v#{version}/LaunchPadder-v#{version}.zip",
+      verified: "github.com/Molay/LaunchPadder/"
   name "LaunchPadder"
-  desc "LaunchPad tool that organizes and sorts apps and folders on LaunchPad according to certain rules"
+  desc "Sorts apps and folders on LaunchPad according to certain rules"
   homepage "https://molayc.com/blog/2022/04/07/launchpadder/"
 
   livecheck do
