351d28a278bfe89f982c3b7e50269a4a4394540f
jfoster <jacobfoster@pm.me>
2022-10-24 10:12:50 +0100

    disable theos bottling

--- Formula/theos.rb
+++ Formula/theos.rb
@@ -4,7 +4,6 @@ class Theos < Formula
   url "https://github.com/theos/theos.git", tag: "2.5"
   sha256 "20539477e6d517917c93914d1c6f2920c172fb006139863675b8ac23505bede5"
   license "GPL-3.0-only"
-  revision 5
   head "https://github.com/theos/theos.git"
 
   livecheck do
@@ -12,11 +11,7 @@ class Theos < Formula
     strategy :github_latest
   end
 
-  bottle do
-    root_url "https://github.com/jfoster/homebrew-tap/releases/download/theos-2.5_2"
-    sha256 cellar: :any_skip_relocation, big_sur:  "fe197625ae318dc02490510e0490b13f6afa408472ce30f5d80cb4a2960d59f6"
-    sha256 cellar: :any_skip_relocation, catalina: "fc428b0a116cbd420ed0da8d08d790c9789c5e1f52c00862836e515ab2f55141"
-  end
+  bottle :unneeded
 
   depends_on "ldid"
   depends_on "xz"
